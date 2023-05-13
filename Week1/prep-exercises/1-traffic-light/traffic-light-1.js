6144e209d383488287552630955012a4151fca2c
gaechkahub <83685230+gaechkahub@users.noreply.github.com>
2023-05-13 12:32:15 +0200

    Solved traffic-light-1.js

--- Week1/prep-exercises/1-traffic-light/traffic-light-1.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-1.js
@@ -9,13 +9,25 @@ const trafficLight = {
 
 let rotations = 0;
 while (rotations < 2) {
-  const currentState = trafficLight.state;
+  let currentState = trafficLight.state;
   console.log("The traffic light is on", currentState);
 
   // TODO
   // if the color is green, turn it orange
+  if (currentState == "green") {
+    currentState = "orange";
+    console.log("The traffic light is on", currentState);
+  }
   // if the color is orange, turn it red
+  if (currentState == "orange") {
+    currentState = "red";
+    console.log("The traffic light is on", currentState);
+  }
   // if the color is red, add 1 to rotations and turn it green
+  if (currentState == "red") {
+    currentState = "green";
+    rotations++;
+  }
 }
 
 /**
