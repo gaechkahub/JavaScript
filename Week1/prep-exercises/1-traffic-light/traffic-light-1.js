09a0b0552f5b143d871dc749885af0c71427addd
gaechkahub <keozon@gmail.com>
2023-05-28 06:08:06 +0200

    minor changes

--- Week1/prep-exercises/1-traffic-light/traffic-light-1.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-1.js
@@ -1,31 +1,31 @@
 "use strict";
 /**
- * The `state` property says what the traffic light's state (i.e. colour) is at
+ * The `state` property says what the traffic light's state (i.e. color) is at
  * that moment.
  */
 const trafficLight = {
-  state: "green",
+	state: "green",
 };
 
 let rotations = 0;
 while (rotations < 2) {
-  let currentState = trafficLight.state;
-  console.log("The traffic light is on", currentState);
+	const currentState = trafficLight.state;
+	console.log("The traffic light is on", currentState);
 
-  // TODO
-  // if the color is green, turn it orange
-  if (currentState === "green") {
-    trafficLight.state = "orange";
-  }
-  // if the color is orange, turn it red
-  else if (currentState === "orange") {
-    trafficLight.state = "red";
-  }
-  // if the color is red, add 1 to rotations and turn it green
-  else {
-    trafficLight.state = "green";
-    rotations++;
-  }
+	// TODO
+	// if the color is green, turn it orange
+	if (currentState === "green") {
+		trafficLight.state = "orange";
+	}
+	// if the color is orange, turn it red
+	else if (currentState === "orange") {
+		trafficLight.state = "red";
+	}
+	// if the color is red, add 1 to rotations and turn it green
+	else {
+		trafficLight.state = "green";
+		rotations++;
+	}
 }
 
 /**
